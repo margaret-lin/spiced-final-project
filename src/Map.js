07d3cc505ff6f945e4f6c6254c65dc922c034c5a
Margaret Lin <mwanyu.lin@gmail.com>
2019-12-17 22:43:51 +0100

    add map paralax scroll

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
+// import Grid from '@material-ui/core/Grid';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Paper from '@material-ui/core/Paper';
 
 const mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 export default function Map() {
     const [viewport, setViewport] = useState({
         width: 100 + 'vw',
-        height: 400,
+        height: 500 + 'px',
         latitude: 37.7577,
         longitude: -122.4376,
         zoom: 3
@@ -22,6 +25,7 @@ export default function Map() {
                     mapboxApiAccessToken={mapboxToken}
                     onViewportChange={onViewportChange}
                     mapStyle='mapbox://styles/maaaama/ck477vjqb2oq91cowyitdnqcm'
+                    scrollZoom={false}
                 />
                 <p>Writing something in map...</p>
             </div>
