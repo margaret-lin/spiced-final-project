b72f8b4be27ea4fa0700d11835b9de6f3c210142
Margaret Lin <mwanyu.lin@gmail.com>
2019-12-17 18:06:36 +0100

    add landing page and routes

---
+++ src/header.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        paddingTop: 10 + 'px',
+        height: 90 + 'px',
+        backgroundColor: '#faf0dc'
+    },
+    logo: {
+        height: 75 + 'px',
+        width: 125 + 'px',
+        marginLeft: 25 + 'px'
+    },
+    button: {
+        fontSize: 20 + 'px',
+        height: 70 + 'px',
+        width: 200 + 'px'
+    }
+}));
+
+export function Header() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={2}>
+                <Grid container item xs={2}>
+                    <Link to='/'>
+                        <img
+                            src='logo.png'
+                            alt='logo'
+                            className={classes.logo}
+                        />
+                    </Link>
+                </Grid>
+                <Grid item xs={2}>
+                    <Link to='/map'>
+                        <Button className={classes.button}>Map</Button>
+                    </Link>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button className={classes.button}>Learn</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button className={classes.button}>Get Involved</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button className={classes.button}>FAQ</Button>
+                </Grid>
+                <Grid item xs={2}>
+                    <Button className={classes.button}>Contact</Button>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
---
+++ src/landing.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: '#FAF0DC',
+        height: 100 + 'vh'
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        height: 'auto',
+        color: theme.palette.text.secondary,
+        opacity: 0.7
+    }
+}));
+
+export function Landing() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root} id='landing-page'>
+            <Grid container spacing={3} id='landing'>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <p>
+                            A paragraph is a series of related sentences
+                            developing a central idea, called the topic. Try to
+                            think about paragraphs in terms of thematic unity: a
+                            paragraph is a sentence or a group of sentences that
+                            supports one central, unified idea. Paragraphs add
+                            one idea at a time to your broader argument.
+                        </p>
+                    </Paper>
+                </Grid>
+                <Grid item xs={7}></Grid>
+                <Grid item xs={7}></Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>
+                        <p>
+                            A paragraph is a series of related sentences
+                            developing a central idea, called the topic. Try to
+                            think about paragraphs in terms of thematic unity: a
+                            paragraph is a sentence or a group of sentences that
+                            supports one central, unified idea. Paragraphs add
+                            one idea at a time to your broader argument.
+                        </p>
+                        <p>
+                            A paragraph is a series of related sentences
+                            developing a central idea, called the topic. Try to
+                            think about paragraphs in terms of thematic unity: a
+                            paragraph is a sentence or a group of sentences that
+                            supports one central, unified idea. Paragraphs add
+                            one idea at a time to your broader argument.
+                        </p>
+                        <p>
+                            A paragraph is a series of related sentences
+                            developing a central idea, called the topic. Try to
+                            think about paragraphs in terms of thematic unity: a
+                            paragraph is a sentence or a group of sentences that
+                            supports one central, unified idea. Paragraphs add
+                            one idea at a time to your broader argument.
+                        </p>
+                    </Paper>
+                </Grid>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <p>
+                            A paragraph is a series of related sentences
+                            developing a central idea, called the topic. Try to
+                            think about paragraphs in terms of thematic unity: a
+                            paragraph is a sentence or a group of sentences that
+                            supports one central, unified idea. Paragraphs add
+                            one idea at a time to your broader argument.
+                        </p>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
